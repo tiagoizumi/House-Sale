4c8cb6bdeea6767708bd575a2653d02756911b35
tiagoizumi <tiagoizumi@gmail.com>
2023-07-17 18:32:57 -0300

    Alteração de param_grid para layer_sequence

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 # 1. Rede neural
 Nesse passo é criado uma rede neural que tem como objetivo predizer o valor das casas com base nos atributos
+Para achar a melhor combinação de número de camadas e número de neurônios em cada camada é implementado um grid search junto a um k-fold cross-validation
 
 # 2. Mapa
 Nesse passo é construido um mapa, que para cada casa tem um marcador de uma cor que indica a precisão da predição do preço:
--- price_prediction.py
+++ price_prediction.py
@@ -15,12 +15,11 @@ X = data.drop('price', axis=1)
 y = data['price']
 
 # Treinamento
-kf = KFold(n_splits=5, shuffle=True)
-param_grid = {'num_layers':[2, 6], 'num_neurons': [16, 64]}
+kf = KFold(n_splits=3, shuffle=True)
+layers_sequences = [[32, 32, 32], [32, 64, 32], [64, 64, 64]]
 input_size = len(X.columns)
 activation_func = 'relu'
-num_epochs = 1
-
+num_epochs = 100
 best_loss = float('inf')
 best_model = None
 best_X_train = None
@@ -29,12 +28,11 @@ best_X_test = None
 best_y_test = None
 
 # Iteração dos hiperparâmetros
-for params in product(param_grid['num_layers'], param_grid['num_neurons']):
-    num_layers, num_neurons = params
+for layer_sequence in layers_sequences:
     # Montagem do modelo
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(input_size, activation='linear', input_shape=(input_size,)))
-    for _ in range(num_layers-1):
+    for num_neurons in layer_sequence:
         model.add(tf.keras.layers.Dense(num_neurons, activation=activation_func))
     model.add(tf.keras.layers.Dense(1, activation='linear'))
     optimizer = tf.keras.optimizers.Adam()
@@ -51,7 +49,7 @@ for params in product(param_grid['num_layers'], param_grid['num_neurons']):
 
         loss = model.evaluate(X_test, y_test, verbose=0)
         losses.append(loss)
-        print(f'Fold {fold+1}, {params}, loss: {loss}')
+        print(f'Fold {fold+1}, {layer_sequence}, loss: {loss}')
         if loss < best_loss:
             best_loss = loss
             best_model = tf.keras.models.clone_model(model)
@@ -59,16 +57,16 @@ for params in product(param_grid['num_layers'], param_grid['num_neurons']):
             best_y_train = y_train
             best_X_test = X_test
             best_y_test = y_test
-            best_param = params
+            best_layer_sequence = layer_sequence
     print(f'Média: {np.mean(losses)} ± {np.std(losses)}')
 
 best_predictions = best_model.predict(best_X_test, verbose=0)
-print('Best params')
-print(best_param)
-print('Predictions')
-print(best_predictions)
-print('Best_y_test')
-print(best_y_test)
+print('Best layer sequence')
+print(best_layer_sequence)
+# print('Predictions')
+# print(best_predictions)
+# print('Best_y_test')
+# print(best_y_test)
 
 
 # Mapas
